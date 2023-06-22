73178f194c97121b93ebe4ffc95b51c8377d62f8
Rcg134 <russelake@gmail.com>
2023-06-22 20:11:52 +0800

    Make script local

--- pages/Front_page_Dental.php
+++ pages/Front_page_Dental.php
@@ -347,11 +347,16 @@
         <i class="fa fa-arrow-up"></i>
       </a>
       
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../static/js/jquery_3.6.min.js"></script>
+    <script src="../static/js/popper.min.js"></script>
+    <script src="../static/js/bootsrap.min.js"></script>
+    <script src="../static/js/all.min.js"></script>
+  
+    <!-- <script src="../static/js/jquery_3.6.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script> -->
     <script src="../static/js/custom.js"></script>
 
 
